b51676eb54361c0c652baec938d9a4929016eabb
M <setiamuhammad527@gmail.com>
2026-04-28 13:58:08 +0700

    nambahin crud di controller kelola anggota

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PeminjamanController;
+use App\Http\Controllers\KelolaAnggotaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/kelola-anggota', [KelolaAnggotaController::class, 'view']);
