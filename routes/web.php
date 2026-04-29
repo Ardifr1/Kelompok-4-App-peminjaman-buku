7420518101fd08d98010283c02c6672ab7aa40d2
アビ (Tippy) <gamerabhi124@gmail.com>
2026-04-29 13:38:33 +0700

    fix merge conflict

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-<<<<<<< HEAD
 use App\Http\Controllers\PeminjamanController;
 use App\Http\Controllers\KelolaAnggotaController;
-
-=======
 use App\Http\Controllers\TestingController;
->>>>>>> 1e34ba8e92fc022510f809dde5e22844162b5899
-=======
 use App\Http\Controllers\KAnggotaController;
 use App\Http\Controllers\LoginController;
-<<<<<<< HEAD
->>>>>>> back-end
-=======
 use App\Http\Controllers\RegisterController;
->>>>>>> front-end
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// Halaman Welcome
 Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+// Route Kelola Anggota (Pastikan method 'view' dan 'index' ada di Controller terkait)
 Route::get('/kelola-anggota', [KelolaAnggotaController::class, 'view']);
-=======
 Route::get('/kelolaAnggota', [KAnggotaController::class, 'index']);
->>>>>>> 1e34ba8e92fc022510f809dde5e22844162b5899
-=======
-Route::get('/kelolaAnggota', [KAnggotaController::class, 'index']);
+
+// Route Autentikasi
 Route::get('/login', [LoginController::class, 'index']);
-<<<<<<< HEAD
->>>>>>> back-end
-=======
-Route::get('/register', [RegisterController::class,'index']);
->>>>>>> front-end
+Route::get('/register', [RegisterController::class, 'index']);
