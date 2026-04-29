57b3fcdaf6989c617c09e65727f64ea43cf3986e
アビ (Tippy) <gamerabhi124@gmail.com>
2026-04-29 11:04:11 +0700

    Registrasi Migrate, Seeder, Model

--- database/seeders/RegistrasiSeeder.php
+++ database/seeders/RegistrasiSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\RegistrasiModel;
 
 class RegistrasiSeeder extends Seeder
 {
@@ -12,7 +13,7 @@ class RegistrasiSeeder extends Seeder
      */
     public function run(): void
     {
-        Registrasi::create([
+        RegistrasiModel::create([
             'NIS' => '12345',
             'Nama' => 'Budi Santoso',
             'Kelas' => 'XII PPLG 1',
