7b88295fbd41641db6754d80e8b28ac94842028e
アビ (Tippy) <gamerabhi124@gmail.com>
2026-04-29 13:28:49 +0700

    Register

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KAnggotaController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/kelolaAnggota', [KAnggotaController::class, 'index']);
 Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class,'index']);
