c21fadbf9fe20c627e8a6cb9f0e4ccf37b611ed7
アビ (Tippy) <gamerabhi124@gmail.com>
2026-04-29 08:25:35 +0700

    here

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TestingController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('Testing', [TestingController::class]);
