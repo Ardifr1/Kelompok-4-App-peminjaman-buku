b9b30cd34b491b193d8002aff581e9707b153978
アビ (Tippy) <gamerabhi124@gmail.com>
2026-05-04 11:55:06 +0700

    regist, Login, Seed, Migrate

--- database/seeders/RegistrasiSeeder.php
+++ database/seeders/RegistrasiSeeder.php
@@ -2,21 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\RegistrasiModel;
+use App\Models\RegistrasiModel; // Pastikan nama model ini benar
+use Illuminate\Support\Facades\Hash;
 
 class RegistrasiSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Jalankan database seeds.
      */
     public function run(): void
     {
         RegistrasiModel::create([
-            'NIS' => '12345',
-            'Nama' => 'Budi Santoso',
-            'Kelas' => 'XII PPLG 1',
-        ]);
+        'nis'      => '12345',
+        'nama'     => 'Budi Santoso',
+        'username' => 'Budi Sentosa',
+        'kelas'    => 'XII PPLG 1',   
+        'password' => Hash::make('password123'),
+    ]);
+
+    // Data kedua
+    RegistrasiModel::create([
+        'nis'      => '12346',
+        'nama'     => 'Abhi Mulki A',
+        'username' => 'Abhi Developer',
+        'kelas'    => 'XII PPLG 1',
+        'password' => Hash::make('rahasia123'),
+    ]);
     }
 }
