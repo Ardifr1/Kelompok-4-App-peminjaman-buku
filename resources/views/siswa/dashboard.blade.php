e829551da43c068a01ecfbcf094fc0df52a0f849
アビ (Tippy) <gamerabhi124@gmail.com>
2026-05-05 13:31:45 +0700

    Migration, Model and Seeder

--- resources/views/siswa/dashboard.blade.php
+++ resources/views/siswa/dashboard.blade.php
@@ -23,8 +23,8 @@
 
     <div class="main-layout">
         <aside class="sidebar">
-            <a href="#" class="menu-item active">Menu Buku</a>
-            <a href="#" class="menu-item">Riwayat</a>
+            <a href="/dashboard" class="menu-item active">Menu Buku</a>
+            <a href="/riwayat" class="menu-item">Riwayat</a>
         </aside>
 
         <main class="content-wrapper">
