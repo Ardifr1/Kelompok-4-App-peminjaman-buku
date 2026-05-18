37c877748e18ec60f25904c17016236417703e3b
M <setiamuhammad527@gmail.com>
2026-05-18 12:47:29 +0700

    nambahin banyak

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\bukuModel;
 
 class AdminDashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.kelola_data');
+        $buku = bukuModel::all();
+        return view('admin.kelola_data', compact('buku'));
     }
 }
